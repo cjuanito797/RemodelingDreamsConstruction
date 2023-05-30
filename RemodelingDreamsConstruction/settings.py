70b9984b0f77fd75a185b0c84d51724facec1583
Juan <jlizarraga@unomaha.edu>
2023-05-30 18:45:25 -0500

    removed bmf from settings.py

--- RemodelingDreamsConstruction/settings.py
+++ RemodelingDreamsConstruction/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "bootstrap_modal_forms",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
