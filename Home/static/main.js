de164d195c5a177c67e9d3762f5e06752f7c1bb0
Juan <jlizarraga@unomaha.edu>
2023-06-12 16:00:26 -0500

    adding scroll padding for testimonials section (mobile).

--- Home/static/main.js
+++ Home/static/main.js
@@ -1,5 +1,6 @@
 const navigationHeight = document.querySelector('#primary-navigation').offsetHeight;
 
+console.log("Height of the navbar is: ", navigationHeight)
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
 
 document.documentElement.style.setProperty('--scroll-intro', navigationHeight + "px");
