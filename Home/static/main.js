5853931781e923c064b26335d82b76ce55188a4f
Juan <jlizarraga@unomaha.edu>
2023-06-12 16:23:24 -0500

    c

--- Home/static/main.js
+++ Home/static/main.js
@@ -5,4 +5,5 @@ console.log("Height of the navbar is: ", navigationHeight)
 const socialNavHeight = document.querySelector('#social_nav').offsetHeight;
 console.log("Height of the social navbar is: ", socialNavHeight)
 
+console.log("Height of both the primary nav and social nav is: ", (navigationHeight + socialNavHeight))
 document.documentElement.style.setProperty('--scroll-padding', (navigationHeight + socialNavHeight) + "px");
